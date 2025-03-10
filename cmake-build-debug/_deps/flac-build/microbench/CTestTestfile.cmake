e2acff3c0d6743dcb7a0087525815fb2a837bf66
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-03-10 03:00:06 -0700

    chore: add cmake files

---
+++ cmake-build-debug/_deps/flac-build/microbench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vismayb/Projects/FunWithSFML/cmake-build-debug/_deps/flac-src/microbench
+# Build directory: /Users/vismayb/Projects/FunWithSFML/cmake-build-debug/_deps/flac-build/microbench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build-debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vismayb/Projects/FunWithSFML/cmake-build-debug/_deps/flac-src/src/libFLAC
+# Build directory: /Users/vismayb/Projects/FunWithSFML/cmake-build-debug/_deps/flac-build/src/libFLAC
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
